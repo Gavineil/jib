e1d997c8d2ba782abdb08a6cff59f3bea780337e
liuqiang <gavineil@outlook.com>
2020-08-05 11:00:05 +0800

    init

---
+++ src/main/java/org/itech/ica/IcaApplication.java
@@ -0,0 +1,13 @@
+package org.itech.ica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/itech/ica/IcaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.itech.ica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

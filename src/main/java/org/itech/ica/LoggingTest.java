9bef655c2e2fea42035a7ce8898ea443ea5d1bba
liuqiang <gavineil@outlook.com>
2020-08-18 10:56:53 +0800

    init

---
+++ src/main/java/org/itech/ica/LoggingTest.java
@@ -0,0 +1,18 @@
+package org.itech.ica;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoggingTest {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingTest.class);
+
+    @Scheduled(fixedDelay = 1000)
+    public void logException() {
+        logger.info("start exception");
+
+        throw new RuntimeException("logging exception");
+    }
+}
